c69d3f289e184eabca783bf4a4631ba7f3dea446
Eben Higgins <ebenezer.higgins@gmail.com>
2026-09-14 15:32:16 -0700

    begin 'quadratic' problem

--- quadratic.py
+++ quadratic.py
@@ -12,14 +12,54 @@ def is_float(x):
     return True
         
 
-def get_terms():
-    ans = input("Enter an equation (use ^ for exponents)\n").split()
+def get_equation():
+    ans = input("Enter an equation (use ^ for exponents)\n")
     if ans:
         return ans
     print("Invalid.")
-    return get_terms()
+    return get_equation()
 
-def parse_equation(terms):
-    for i in range(len(terms)):
-        if is_float(terms[i]):
-            terms[i] = float(term)
+def get_type(a):
+    if a.isalpa():
+        return "letter"
+    elif a.is_float():
+        return "number"
+    elif a == "+" or a == "-" or a == "^" or a == '/' or a == "*":
+        return "symbol"
+    else:
+        return "none"
+    
+
+def parse_equation(eq):
+    var = ""
+    terms = []
+    term = ""
+    last_type = "none"
+
+    for a in eq:
+        ty = get_type(a)
+
+        match ty:
+            case "letter":
+                if var == "":
+                    var = a
+                elif a == var:
+                    pass
+                else:
+                    return False
+            case "number" if last_type == "number":
+                term += a
+            case "number" if last_type != "number":
+                # add old term to list and clear term varraible
+                term
+
+        
+def calculate_quadratic():
+    works = False
+    while not works:
+        terms = get_equation()
+        parsed = parse_equation(terms)
+        if parsed:
+            works = True
+        else:
+            print("Invalid.")
