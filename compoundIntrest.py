31dbb53a5d0cff13fceb57d4f8e88907d4221be5
Electronic-Ben <ebenezer.higgins@gmail.com>
2026-09-18 14:14:58 -0700

    Completed compund intrest problem

---
+++ compoundIntrest.py
@@ -0,0 +1,23 @@
+def get_float(msg, fail = "Invalid."):
+    try:
+        ans = float(input(msg))
+    except Exception as e:
+        print(fail)
+        return get_float(msg, fail)
+    return ans
+
+def get_inputs():
+    p = get_float("Starting amount in account: ")
+    r = get_float("Annual intrest as decimal: ")
+    n = get_float("Times compounded per year: ")
+    t = get_float("Number of years: ")
+    return [p, r, n, t]
+
+def calculate_intrest():
+    [p, r, n, t] = get_inputs()
+
+    amount = p * ((1+ (r/n))**(n*t))
+
+    print("Expected intrest: " + str(round(amount, 2)))
+
+calculate_intrest()
